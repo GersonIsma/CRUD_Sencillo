e5fd75ffceb589639178a63ccb9deda6b89db27a
Gerson Flores <157324488+GersonIsma@users.noreply.github.com>
2025-07-09 23:15:36 -0600

    Add files via upload
    primer commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    nombre = request.args.get('nombre')
+    users = []
+    if nombre:
+        users = User.query.filter(User.name.ilike(f"%{nombre}%")).all()
+    return render_template('home.html', users=users)
+
+@app.route('/agregar', methods=['GET', 'POST'])
+def agregar():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        db.session.add(User(name=name, email=email))
+        db.session.commit()
+        return redirect('/')
+    return render_template('agregar.html')
+
+@app.route('/consultar')
+def consultar():
+    nombre = request.args.get('nombre')
+    ver_todos = request.args.get('todos')
+    users = []
+
+    if nombre:
+        users = User.query.filter(User.name.ilike(f"%{nombre}%")).all()
+    elif ver_todos:
+        users = User.query.all()
+
+    return render_template('consultar.html', users=users)
+
+@app.route('/consultar/<int:id>')
+def consultar_uno(id):
+    user = User.query.get_or_404(id)
+    return render_template('ver_usuario.html', user=user)
+
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    user = User.query.get_or_404(id)
+    if request.method == 'POST':
+        user.name = request.form['name']
+        user.email = request.form['email']
+        db.session.commit()
+        return redirect('/consultar')
+    return render_template('editar.html', user=user)
+
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect('/consultar')
+
+if __name__ == '__main__':
+    app.run()
